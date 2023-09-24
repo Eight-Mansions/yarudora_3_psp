3155c2662f42aff3307a4a1b9d277d55fabddbfa
esperknight <esperknight@yahoo.com>
2023-09-24 01:05:16 -0400

    Setup insertion!

---
+++ 2_insert.bat
@@ -0,0 +1,94 @@
+del error.txt
+del /s /q ins\*
+xcopy /s orig\* ins\*
+
+echo ins\S001\SCN001.SSC.txt >> error.txt
+tools\atlas ins\S001\SCN001.SSC trans\S001\SCN001.SSC.txt >> error.txt
+echo ins\S002\SCN002.SSC.txt >> error.txt
+tools\atlas ins\S002\SCN002.SSC trans\S002\SCN002.SSC.txt >> error.txt
+echo ins\S003\SCN003.SSC.txt >> error.txt
+tools\atlas ins\S003\SCN003.SSC trans\S003\SCN003.SSC.txt >> error.txt
+echo ins\S004\SCN004.SSC.txt >> error.txt
+tools\atlas ins\S004\SCN004.SSC trans\S004\SCN004.SSC.txt >> error.txt
+echo ins\S005\SCN005.SSC.txt >> error.txt
+tools\atlas ins\S005\SCN005.SSC trans\S005\SCN005.SSC.txt >> error.txt
+echo ins\S006\SCN006.SSC.txt >> error.txt
+tools\atlas ins\S006\SCN006.SSC trans\S006\SCN006.SSC.txt >> error.txt
+echo ins\S007\SCN007.SSC.txt >> error.txt
+tools\atlas ins\S007\SCN007.SSC trans\S007\SCN007.SSC.txt >> error.txt
+echo ins\S008\SCN008.SSC.txt >> error.txt
+tools\atlas ins\S008\SCN008.SSC trans\S008\SCN008.SSC.txt >> error.txt
+echo ins\S009\SCN009.SSC.txt >> error.txt
+tools\atlas ins\S009\SCN009.SSC trans\S009\SCN009.SSC.txt >> error.txt
+echo ins\S00A\SCN00A.SSC.txt >> error.txt
+tools\atlas ins\S00A\SCN00A.SSC trans\S00A\SCN00A.SSC.txt >> error.txt
+echo ins\S00B\SCN00B.SSC.txt >> error.txt
+tools\atlas ins\S00B\SCN00B.SSC trans\S00B\SCN00B.SSC.txt >> error.txt
+echo ins\S00C\SCN00C.SSC.txt >> error.txt
+tools\atlas ins\S00C\SCN00C.SSC trans\S00C\SCN00C.SSC.txt >> error.txt
+echo ins\S00D\SCN00D.SSC.txt >> error.txt
+tools\atlas ins\S00D\SCN00D.SSC trans\S00D\SCN00D.SSC.txt >> error.txt
+echo ins\S00E\SCN00E.SSC.txt >> error.txt
+tools\atlas ins\S00E\SCN00E.SSC trans\S00E\SCN00E.SSC.txt >> error.txt
+echo ins\S00F\SCN00F.SSC.txt >> error.txt
+tools\atlas ins\S00F\SCN00F.SSC trans\S00F\SCN00F.SSC.txt >> error.txt
+echo ins\S010\SCN010.SSC.txt >> error.txt
+tools\atlas ins\S010\SCN010.SSC trans\S010\SCN010.SSC.txt >> error.txt
+echo ins\S011\SCN011.SSC.txt >> error.txt
+tools\atlas ins\S011\SCN011.SSC trans\S011\SCN011.SSC.txt >> error.txt
+echo ins\S012\SCN012.SSC.txt >> error.txt
+tools\atlas ins\S012\SCN012.SSC trans\S012\SCN012.SSC.txt >> error.txt
+echo ins\S013\SCN013.SSC.txt >> error.txt
+tools\atlas ins\S013\SCN013.SSC trans\S013\SCN013.SSC.txt >> error.txt
+echo ins\S014\SCN014.SSC.txt >> error.txt
+tools\atlas ins\S014\SCN014.SSC trans\S014\SCN014.SSC.txt >> error.txt
+echo ins\S015\SCN015.SSC.txt >> error.txt
+tools\atlas ins\S015\SCN015.SSC trans\S015\SCN015.SSC.txt >> error.txt
+echo ins\S016\SCN016.SSC.txt >> error.txt
+tools\atlas ins\S016\SCN016.SSC trans\S016\SCN016.SSC.txt >> error.txt
+echo ins\S017\SCN017.SSC.txt >> error.txt
+tools\atlas ins\S017\SCN017.SSC trans\S017\SCN017.SSC.txt >> error.txt
+echo ins\S018\SCN018.SSC.txt >> error.txt
+tools\atlas ins\S018\SCN018.SSC trans\S018\SCN018.SSC.txt >> error.txt
+echo ins\S019\SCN019.SSC.txt >> error.txt
+tools\atlas ins\S019\SCN019.SSC trans\S019\SCN019.SSC.txt >> error.txt
+echo ins\S01A\SCN01A.SSC.txt >> error.txt
+tools\atlas ins\S01A\SCN01A.SSC trans\S01A\SCN01A.SSC.txt >> error.txt
+echo ins\S01B\SCN01B.SSC.txt >> error.txt
+tools\atlas ins\S01B\SCN01B.SSC trans\S01B\SCN01B.SSC.txt >> error.txt
+echo ins\S01C\SCN01C.SSC.txt >> error.txt
+tools\atlas ins\S01C\SCN01C.SSC trans\S01C\SCN01C.SSC.txt >> error.txt
+echo ins\S01D\SCN01D.SSC.txt >> error.txt
+tools\atlas ins\S01D\SCN01D.SSC trans\S01D\SCN01D.SSC.txt >> error.txt
+echo ins\S01E\SCN01E.SSC.txt >> error.txt
+tools\atlas ins\S01E\SCN01E.SSC trans\S01E\SCN01E.SSC.txt >> error.txt
+echo ins\S01F\SCN01F.SSC.txt >> error.txt
+tools\atlas ins\S01F\SCN01F.SSC trans\S01F\SCN01F.SSC.txt >> error.txt
+echo ins\S020\SCN020.SSC.txt >> error.txt
+tools\atlas ins\S020\SCN020.SSC trans\S020\SCN020.SSC.txt >> error.txt
+echo ins\S021\SCN021.SSC.txt >> error.txt
+tools\atlas ins\S021\SCN021.SSC trans\S021\SCN021.SSC.txt >> error.txt
+echo ins\S022\SCN022.SSC.txt >> error.txt
+tools\atlas ins\S022\SCN022.SSC trans\S022\SCN022.SSC.txt >> error.txt
+echo ins\S023\SCN023.SSC.txt >> error.txt
+tools\atlas ins\S023\SCN023.SSC trans\S023\SCN023.SSC.txt >> error.txt
+echo ins\S024\SCN024.SSC.txt >> error.txt
+tools\atlas ins\S024\SCN024.SSC trans\S024\SCN024.SSC.txt >> error.txt
+echo ins\S025\SCN025.SSC.txt >> error.txt
+tools\atlas ins\S025\SCN025.SSC trans\S025\SCN025.SSC.txt >> error.txt
+echo ins\S026\SCN026.SSC.txt >> error.txt
+tools\atlas ins\S026\SCN026.SSC trans\S026\SCN026.SSC.txt >> error.txt
+echo ins\S027\SCN027.SSC.txt >> error.txt
+tools\atlas ins\S027\SCN027.SSC trans\S027\SCN027.SSC.txt >> error.txt
+echo ins\S028\SCN028.SSC.txt >> error.txt
+tools\atlas ins\S028\SCN028.SSC trans\S028\SCN028.SSC.txt >> error.txt
+echo ins\S029\SCN029.SSC.txt >> error.txt
+tools\atlas ins\S029\SCN029.SSC trans\S029\SCN029.SSC.txt >> error.txt
+echo ins\SSC\MAIN.SSC.txt >> error.txt
+tools\atlas ins\SSC\MAIN.SSC trans\SSC\MAIN.SSC.txt >> error.txt
+echo ins\SSC\SCN090.SSC.txt >> error.txt
+tools\atlas ins\SSC\SCN090.SSC trans\SSC\SCN090.SSC.txt >> error.txt
+echo ins\SSC\SCN091.SSC.txt >> error.txt
+tools\atlas ins\SSC\SCN091.SSC trans\SSC\SCN091.SSC.txt >> error.txt
+echo ins\SSC\STARTUP.SSC.txt >> error.txt
+tools\atlas ins\SSC\STARTUP.SSC trans\SSC\STARTUP.SSC.txt >> error.txt
